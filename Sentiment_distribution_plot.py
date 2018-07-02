a165208ede8e59c07f4a8b6c84a30d4fa37eb9d9
faznt <39524443+faznt@users.noreply.github.com>
2018-07-02 14:34:56 +0200

    b

--- Sentiment_distribution_plot.py
+++ Sentiment_distribution_plot.py
@@ -13,31 +13,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-database = access.db
-conversationList = []
 
-airlines_id = ["56377143", "106062176", "18332190", "22536055", "124476322", "26223583", "2182373406", "38676903",
-               "1542862735", "253340062", "218730857", "45621423", "20626359"]
-airlines_names = ["KLM", "AirFrance", "British_Airways", "AmericanAir", "Lufthansa", "AirBerlin", "AirBerlin assist",
-                  "easyJet", "RyanAir", "SingaporeAir", "Qantas", "EtihadAirways", "VirginAtlantic"]
-airlines_other_id = ["56377143", "106062176", "18332190", "124476322", "26223583", "2182373406", "38676903",
-                     "1542862735", "253340062", "218730857", "45621423", "20626359"]
-airlines_other_names = ["KLM", "AirFrance", "British_Airways", "Lufthansa", "AirBerlin", "AirBerlin assist", "easyJet",
-                        "RyanAir", "SingaporeAir", "Qantas", "EtihadAirways", "VirginAtlantic"]
-
-# Default for AA
-user_id = airlines_id[3]
-user_name = airlines_names[3]
-
-disclist = []
 df3 = pd.read_csv('labeled_sentiment.csv', low_memory=False)
-
-
-    
-for airline in df3:
-    df3[airline].value_counts()
-#    df['KLM'].value_counts()
-
 df1 = df3.apply(pd.value_counts)    
 print(df1)
 #Gather the value counts with the above code.
